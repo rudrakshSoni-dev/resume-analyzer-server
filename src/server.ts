053b07865252ee336889bb1e6ab7d57c72e8ba37
Rudraksh <mrrssoni12@gmail.com>
2026-04-12 15:48:37 +0530

    changes in server

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import app from "./app";
 import prisma from "./prisma/client";
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 const cloudinary_config = cloudinary.config();
 console.log("Cloudinary configured:", !!cloudinary_config.cloud_name);
 
@@ -16,7 +16,7 @@ async function startServer() {
     await prisma.$connect();
     console.log("Database connected");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running at http://localhost:${PORT}`);
     });
         
